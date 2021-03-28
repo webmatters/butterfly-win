3e3381ce7320f501ef90aa0b150a89eaac42406c
Andrew Smith <andrew.smith@webmatters.io>
2021-03-28 11:36:45 -0400

    Testing Vercel preview deployments from test branch.

--- pages/about.js
+++ pages/about.js
@@ -7,6 +7,10 @@ function About() {
     <PageLayout>
       <div>
         <h1>About page</h1>
+        <div>
+          Testing to see if pushing to the 'test' branch creates a preview
+          deployment in Vercel.
+        </div>
       </div>
     </PageLayout>
   )
