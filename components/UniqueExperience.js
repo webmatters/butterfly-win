f29dcb4925ad0143f7711e6e041f33dc940c3362
webmatters <andrew.smith@webmatters.io>
2021-04-18 12:15:30 -0400

    Changes to home page.

--- components/UniqueExperience.js
+++ components/UniqueExperience.js
@@ -2,13 +2,13 @@ import Link from 'next/link'
 
 function UniqueExperience() {
   return (
-    <div className="mb-12 pt-24 pb-12 bg-white">
+    <div className="pt-24 pb-12 bg-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-16 text-center">
         <div className="lg:text-center">
           {/* <h2 className="text-base text-primary font-semibold tracking-wide uppercase">
             Our Unique Experience
           </h2> */}
-          <p className="mt-2 text-3xl leading-8 font-semibold tracking-tight text-gray-700 sm:text-4xl">
+          <p className="mt-2 text-3xl leading-8 font-semibold tracking-tight text-primary sm:text-4xl">
             Our Unique Experience
           </p>
           {/* <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
@@ -22,12 +22,12 @@ function UniqueExperience() {
               where you can lay on the table and relax while the practitioner
               does all the work, I find that getting my clients involved in the
               experience brings them{' '}
-              <span className="font-cursive text-2xl font-bold">
+              <span className="font-cursive text-2xl font-bold text-primary-dark">
                 {' '}
                 more in touch{' '}
               </span>{' '}
               with themselves and helps them to learn ways to{' '}
-              <span className="font-cursive text-2xl font-bold">
+              <span className="font-cursive text-2xl font-bold text-primary-dark">
                 {' '}
                 maintain balance{' '}
               </span>{' '}
@@ -38,7 +38,7 @@ function UniqueExperience() {
               In my sessions there are times where you will relax on the table,
               and others where you may be holding or rubbing points,
               participating in muscle tests or examining
-              <span className="font-cursive text-2xl font-bold">
+              <span className="font-cursive text-2xl font-bold text-primary-dark">
                 {' '}
                 emotions{' '}
               </span>{' '}
@@ -46,7 +46,7 @@ function UniqueExperience() {
               process is the same for each session, but the techniques used to
               balance and the experience of the session are unique to you and
               your needs that day. In all sessions, the goal is that{' '}
-              <span className="font-cursive text-2xl font-bold">
+              <span className="font-cursive text-2xl font-bold text-primary-dark">
                 {' '}
                 you feel better{' '}
               </span>{' '}
@@ -55,7 +55,7 @@ function UniqueExperience() {
             <p className="mb-8">
               One of the most amazing things about energy work is that you
               really can make{' '}
-              <span className="font-cursive text-2xl font-bold">
+              <span className="font-cursive text-2xl font-bold text-primary-dark">
                 {' '}
                 lasting change{' '}
               </span>{' '}
@@ -63,7 +63,7 @@ function UniqueExperience() {
               be improved, and pain can often be reduced or relieved in a matter
               of minutes or seconds. In both my sessions and classes I make sure
               there is both
-              <span className="font-cursive text-2xl font-bold">
+              <span className="font-cursive text-2xl font-bold text-primary-dark">
                 {' '}
                 healing and learning{' '}
               </span>{' '}
@@ -73,7 +73,7 @@ function UniqueExperience() {
             </p>
             <p className="mb-8">
               Energy work is based in science and it{' '}
-              <span className="font-cursive text-2xl font-bold">
+              <span className="font-cursive text-2xl font-bold text-primary-dark">
                 {' '}
                 feels like magic{' '}
               </span>{' '}
@@ -84,13 +84,6 @@ function UniqueExperience() {
               imagined.
             </p>
           </div>
-          {/* <div className="flex justify-center mt-10">
-            <Link href="/classes">
-              <a className="px-8 py-2 border border-transparent text-base font-medium rounded-full text-white bg-primary md:py-3 md:text-lg md:px-10 btn">
-                Sign Up
-              </a>
-            </Link>
-          </div> */}
         </div>
       </div>
     </div>
